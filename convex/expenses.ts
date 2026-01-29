578baf5b364e85a7d5dbc5669d9352addb619850
AnganSamadder <rio.angan@gmail.com>
2026-01-29 16:06:35 -0500

    perf(expenses): switch list query to fan-out table

--- convex/expenses.ts
+++ convex/expenses.ts
@@ -1,6 +1,5 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
-import { getAllEquivalentMemberIds } from "./aliases";
 import { reconcileUserExpenses } from "./helpers";
 
 async function getCurrentUser(ctx: any) {
